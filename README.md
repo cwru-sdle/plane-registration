9327fde0b01346e389098cbf3ecb513c27192b7c
Anthony Lino <aml334@case.edu>
2025-05-20 13:33:02 -0400

    directories added

--- README.md
+++ README.md
@@ -2,5 +2,34 @@
 
 This repo has the scripts used to identify the x,y,z position of a defect from a cross section, such as metallography, EBSD, TEM, etc from the part
 
-
 NOTE: The center is in the top left corner for images
+
+# Coordinates
+
+Here we are finding the xyz coordiantes from the metlalography samples. This is a little confusing so bear with me:
+
+1. We load the following packages:
+
+   1. roifil - to open the coordinates files from imagej
+   2. os - for pathing
+   3. numpy - for numeric operations
+   4. cv2 - for momentums
+   5. scipy - for root solving
+   6. jupyter notebook - to run a script as a notebook
+2. We load the measurements for the part edge lengths.
+
+   1. We load every line and calculate their length in the folder and save it as a list of tuples
+   2. We find the roi labeled 1 mm and calculate the factor to convert pixel length to millimeters
+   3. Convert the line lengths into millimeters
+3. Calculate alpha from the lengths
+
+   1. Define the system of equations, which here is delta z, x_center and tan(alpha)
+   2. Calculate theta
+   3. Apply apha to the measurements. Here 1 is always the left side of the face any 2 is always the right side
+4. Load the enscribining ellipses
+
+   1. If the roi is a default, it is appended to a list, otherwise, it is put into a dictionary
+   2. We find the roi labeled 1 mm and calculate the factor to convert pixel length to millimeters
+   3. The center point is found by averaging the positions of all holes
+   4. phi is calculate from these center points
+   5.
--- coordinates.py
+++ coordinates.py
@@ -5,9 +5,11 @@ import os
 import numpy as np
 import cv2
 import scipy
+import pandas as pd
+data_directory = "/home/anthony-lino/repos/plane-registration/data/"
 # %%
 '''Load Lines'''
-directory = "/home/anthony-lino/repos/plane-registration/samples/sample2/20/rois/"
+directory = data_directory+"microscope/sample2/20/rois/"
 lines = []
 for file in os.listdir(directory):
     if ".roi" in file:
@@ -21,12 +23,12 @@ for file in os.listdir(directory):
         )
 for line in lines:
     if '1mm' in line[1]:
-        mm_scale = 1/line[0]
+        mm_scale_20x = 1/line[0]
 lines_lengths = []
 for line in lines:
     lines_lengths.append(
         (
-            line[0]*mm_scale,
+            line[0]*mm_scale_20x,
             line[1].replace(".roi","")
 
         )
@@ -46,10 +48,10 @@ def alpha_equation(alpha,m1,m2, theta,triangle1,b):
         if m2>m1:
             x_center=-x_center
     return np.tan(alpha)-(delta_z/2)/((b/2)+x+x_center)
-L = (100 - b)/2
-b = 50
-h = 100
-THETA = np.arctan((100/2)/(50/2))
+b = 5
+h = 10
+L = (10 - b)/2
+THETA = np.arctan((h/2)/(b/2))
 alpha = {
     'top': scipy.optimize.fsolve(alpha_equation, x0=0, args=(m_lengths['top_1'],m_lengths['top_2'], THETA,True,b)),
     'bottom': scipy.optimize.fsolve(alpha_equation, x0=0, args=(m_lengths['bottom_1'],m_lengths['bottom_2'], THETA,True,b)),
@@ -58,8 +60,8 @@ alpha = {
 }
 # %%
 '''Load Ellipses'''
-directory= "/home/anthony-lino/repos/plane-registration/samples/sample2/500/rois/"
-centroid = []
+directory= data_directory+"microscope/sample2/500/rois/"
+centroid_500x = []
 holes = []
 for file in os.listdir(directory):
     roi = roifile.roiread(directory+file)
@@ -68,9 +70,9 @@ for file in os.listdir(directory):
         M = cv2.moments(array)
         cx = M["m10"] / M["m00"]
         cy = M["m01"] / M["m00"]
-        centroid.append(np.array([cx, cy]))
+        centroid_500x.append(np.array([cx, cy]))
     elif '1mm' in file:
-        mm_scale - 1/np.linalg.norm(array[0]-array[1])
+        mm_scale_500x = 1/np.linalg.norm(array[0]-array[1])
     else:
         M = cv2.moments(array)
         cx = M["m10"] / M["m00"]
@@ -96,6 +98,13 @@ image_angles = {
     'left':np.arctan(image_deltas['left'][0]/image_deltas['left'][1]),
     'right':np.arctan(image_deltas['right'][0]/image_deltas['right'][1])
 }
+phi = np.mean([
+    image_angles['top'],
+    image_angles['bottom'],
+    image_angles['left'],
+    image_angles['right']
+    ], axis=0
+)
 center = np.mean([
     holes['bottom_left'],
     holes['bottm_right'],
@@ -106,17 +115,17 @@ center = np.mean([
 )
 # %%
 '''z center'''
-def z_equation(alpha,m1,m2,L):
+def z_equation(alpha,m1,m2,L=(10 - b)/2,theta=np.radians(60)):
     x1 = (m1*np.cos(alpha))-L
     x2 = (m2*np.cos(alpha))-L
-    z1 = np.tan(THETA)
-    z2 = (x2+L)*np.sin(alpha)
+    z1 = np.tan(theta)*x1
+    z2 = np.tan(theta)*x2
     return np.mean([z1,z2])
 z_positions = {
-    'left':z_equation(alpha=alpha['left'],m1=m_lengths['left_1'],m2=m_lengths['left_2'],L=(100 - b)/2),
-    'right':z_equation(alpha=alpha['right'],m1=m_lengths['right_1'],m2=m_lengths['right_2'],L=(100 - b)/2),
-    'bottom':z_equation(alpha=alpha['bottom'],m1=m_lengths['bottom_1'],m2=m_lengths['bottom_2'],L=(100 - b)/2),
-    'top':z_equation(alpha=alpha['top'],m1=m_lengths['top_1'],m2=m_lengths['top_2'],L=(100 - b)/2),
+    'left':z_equation(alpha=alpha['left'],m1=m_lengths['left_1'],m2=m_lengths['left_2'],),
+    'right':z_equation(alpha=alpha['right'],m1=m_lengths['right_1'],m2=m_lengths['right_2']),
+    'bottom':z_equation(alpha=alpha['bottom'],m1=m_lengths['bottom_1'],m2=m_lengths['bottom_2']),
+    'top':z_equation(alpha=alpha['top'],m1=m_lengths['top_1'],m2=m_lengths['top_2']),
 }
 z_center = np.mean([
     z_positions['left'],
@@ -125,13 +134,81 @@ z_center = np.mean([
     z_positions['bottom']
     ], axis=0
 )
+#%%
+'''Find Position'''
+def z_position(position, alpha, beta, z_center,phi):
+    x_CAD = position[0]*np.cos(phi)+position[1]*np.sin(phi)
+    y_CAD = -position[0]*np.sin(phi)+position[1]*np.cos(phi)
+    z = z_center + x_CAD * np.tan(alpha) + y_CAD * np.tan(beta)
+    return [x_CAD, y_CAD, z]
+
+# Apply the transformation
+position_list = list(map(
+    lambda x: z_position((x - center) * mm_scale_500x, alpha['top'], alpha['left'], z_center,phi),
+    centroid_500x
+))
+
+# Convert to DataFrame
+df = pd.DataFrame(position_list, columns=['x', 'y', 'z'])
+df['z'] = df['z'].apply(lambda x: x[0]) # Saves as float not list
+
+# Save to CSV (or any other format)
+df.to_csv(data_directory+'csvs/500x_positions.csv', index=False)
 # %%
-'''Print Results'''
-print("ANGLES\n"+"-"*10)
-print(alpha)
-print("COORDINATES\n"+"-"*10)
-print(center_values)
-print("IMAGE_ANGLES\n"+"-"*10)
-print(image_angles)
-#print("DEFECTS\n"+"-"*10)
-#print(centroid)
+directory = data_directory + "microscope/sample2/150/"
+zip_path = os.path.join(directory, "RoiSet.zip")
+centroid_150x = []
+centers_150x = []
+for roi in roifile.roiread(zip_path):
+    if roi.name=="1mm":
+        array = np.array([[roi.x1, roi.y1],[roi.x2, roi.y2]])
+        mm_scale_150x = 1/np.linalg.norm(array[0]-array[1])
+    
+    elif '-' in roi.name:
+        center_x = (roi.left + roi.right) / 2
+        center_y = (roi.top + roi.bottom) / 2
+        centroid_150x.append(np.array([center_x,center_y]))
+    elif 'top' in roi.name or 'bottom' in roi.name or 'middle' in roi.name:
+        center_x = (roi.left + roi.right) / 2
+        center_y = (roi.top + roi.bottom) / 2
+        centers_150x.append(
+            (
+                np.array(
+                    [center_x,center_y]
+                ),
+                roi.name
+            )
+        )        
+    else:
+        print("ROI type not specifically handled.")
+centers_150x= {name:val for val, name in centers_150x}
+#%%
+center_150x = np.mean(
+    [
+        centers_150x['top_left'],
+        centers_150x['top_right'],
+        centers_150x['middle'],
+        centers_150x['bottom_right'],
+        centers_150x['bottom_left']
+    ], axis=0
+)
+#%%
+'''Find Position'''
+def z_position(position, alpha, beta, z_center,phi):
+    x_CAD = position[0]*np.cos(phi)+position[1]*np.sin(phi)
+    y_CAD = -position[0]*np.sin(phi)+position[1]*np.cos(phi)
+    z = z_center + x_CAD * np.tan(alpha) + y_CAD * np.tan(beta)
+    return [x_CAD, y_CAD, z]
+
+# Apply the transformation
+position_list = list(map(
+    lambda x: z_position((x - center_150x) * mm_scale_150x, alpha['top'], alpha['left'], z_center,phi),
+    centroid_150x
+))
+
+# Convert to DataFrame
+df = pd.DataFrame(position_list, columns=['x', 'y', 'z'])
+df['z'] = df['z'].apply(lambda x: x[0]) # Saves as float not list
+
+# Save to CSV (or any other format)
+df.to_csv(data_directory+'csvs/150x_positions.csv', index=False)
